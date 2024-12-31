911604c8193ebf3555120f1858ebde216e398b3b
Jayendra2688 <jayendrareddy2688@gmail.com>
2024-12-31 19:27:51 +0530

    differnt routes

--- sever.js
+++ sever.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const app = express();
 
-app.get('/',function(res,req){
-    req.send("<h1>Hello</h1>");
+app.get('/',function(req,res){
+    res.send("<h1>Hello</h1>");
 })
-
+app.get('/about',function(req,res){
+    res.send("Hey,I am Jayendra Reddy!!");
+})
 app.listen(3000,function(){
     console.log("server is listening on port 3000.");
 });
