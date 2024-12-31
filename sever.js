df55062f822ef7012c2a6f7aae6cf0da44a568b7
Jayendra2688 <jayendrareddy2688@gmail.com>
2024-12-31 19:12:21 +0530

    second commit serving get requests

--- sever.js
+++ sever.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/',function(res,req){
+    req.send("<h1>Hello</h1>");
+})
+
 app.listen(3000,function(){
     console.log("server is listening on port 3000.");
 });
